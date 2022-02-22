d2997dffd5fd528c34f85fe1da496261ee5a95af
nixbytes <real8686@gmail.com>
2022-02-21 21:01:45 -0500

    add final touch

--- main.go
+++ main.go
@@ -7,26 +7,51 @@ import (
   "strings"
   "strconv"
   "encoding/csv"
+  "encoding/json"
+  "io"
 )
 
 type User struct {
-  id    int    `json:"id"`
-  Name  string `json:"name"`
-  Home  string `json:"home"`
+  Id int `json:"id"`
+  Name string `json:"name"`
+  Home string `json:"home"`
   Shell string `json:"shell"`
 }
 
 func main() {
-  fmt.Println("basic setup")
+  path, format := parseFlags()
+  users := collectUsers()
+
+  var output io.Writer
+
+  if path != "" {
+    f, err := os.Create(path)
+    handleError(err)
+    defer f.Close()
+    output = f
+  } else {
+      output = os.Stdout
+  }
+
+  if format == "json" {
+    data, err := json.MarshalIndent(users, "", "  ")
+    handleError(err)
+    output.Write(data)
+  } else if format == "csv" {
+
+    output.Write([]byte("name,id,home,shell\n"))
+    writer := csv.NewWriter(output)
+    for _, user := range users {
+      err := writer.Write([]string{user.Name, strconv.Itoa(user.Id), user.Home, user.Shell})
+      handleError(err)
+    }
+    writer.Flush()
+  }
 }
 
-
-func parseFlags()(path, format string){
-
-  flag.StringVar(&path, "path","","the path to export file")
-  flag.StringVar(&format, "json","","the output format for the user information. Available optio
-ns are 'csv' and 'json'. The default option is json.")
-
+func parseFlags() (path, format string) {
+  flag.StringVar(&path, "path", "", "the path to the export file.")
+  flag.StringVar(&format, "format", "json", "the output format for the user information. Available options are 'csv' and 'json'. The default format is json.")
   flag.Parse()
 
   format = strings.ToLower(format)
@@ -34,7 +59,48 @@ ns are 'csv' and 'json'. The default option is json.")
   if format != "csv" && format != "json" {
     fmt.Println("Error: invalid format. Use 'json' or 'csv' instead.")
     flag.Usage()
-    oss.Exit(1)
+    os.Exit(1)
+  }
+
+  return
+
+}
+
+func handleError(err error) {
+  if err != nil {
+    fmt.Println("Error:", err)
+    os.Exit(1)
+  }
+}
+
+// <CLOUD_USER_VALUE_FROM_ETC/PASSWD>
+func collectUsers() (users []User) {
+  f, err := os.Open("/etc/passwd")
+  handleError(err)
+  defer f.Close()
+
+  reader := csv.NewReader(f)
+  reader.Comma = ':'
+
+  lines, err := reader.ReadAll()
+  handleError(err)
+
+  for _, line := range lines {
+    id, err := strconv.ParseInt(line[2], 10, 64)
+    handleError(err)
+
+    if id < 1000 {
+      continue
+    }
+
+    user := User{
+      Name: line[0],
+      Id: int(id),
+      Home: line[5],
+      Shell: line[6],
+    }
+
+    users = append(users, user)
   }
 
   return
